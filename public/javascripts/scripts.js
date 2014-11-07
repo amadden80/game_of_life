908dd4596272ab9705c9fcaddfcfd99c37f4e028
amadden80 <amadden80@gmail.com>
2014-11-07 10:13:21 -0500

    let there be random beautiful life

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -6,6 +6,9 @@ var $canvas;  // Canvas Node... this is an html node
 var context;  // Canvas Context... this is my connection to my canvas... WITH all the functions needed to draw
 var numRows = 50;  // How many rows?
 var numColumns = 50;  // How many columns?
+var blockSize = 15;  // How many pixels should each square cell be?
+var aliveColor = 'rgb(145, 45, 45)';  // What should a living cell look like?
+var blockColor = 'rgb(255, 255, 255)';  // What should a non-living cell look like?
 var worldState;  //  What is the current state of the world?
 
 
@@ -19,11 +22,21 @@ function worldGen(){                       // pretend numRows=3 & numColumns=2
   return localWorld;
 }
 
+// *** Random Cell Birth **
+function letThereBeLife(numBorn) {
+  for (var i = 0; i < numBorn; i++) {   // numBorn number of times...
+    var rowIndex = Math.floor(Math.random()*numRows); // Row: Randomly selected
+    var colIndex = Math.floor(Math.random()*numColumns); // Column: Randomly selected
+    worldState[rowIndex][colIndex] = true;  // It has life!
+  }
+  return worldState;
+}
+
 
 $(function(){
 
   worldState = worldGen(); // "And then there was the world"
-  //  "Let there be life"..
+  letThereBeLife(10); //  "Let there be life"..
 
   $canvas = $('#board');
   context = $canvas[0].getContext('2d');
